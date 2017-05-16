3ab41ff5884e85400d79d4839d3ba0b8140a1ef8
Chloe Choi <chloe.meowc@gmail.com>
2017-05-16 18:11:55 -0400

    fixed

---
+++ components/UserGate.js
@@ -0,0 +1,24 @@
+import { firebaseApp } from '../Home';
+import styles from './style/styles.js';
+import RenderVideoTest from '../FeatureTests/RenderVideoTest'
+import Faker from 'Faker';
+import React, { Component } from 'react';
+import { Button, Container, Content, Text, Form, Item, Label, Input, Header, Left, Right, Title, Icon, Body } from 'native-base';
+import {
+    TextInput,
+    View,
+    AppRegistry,
+    ListView,
+    TouchableHighlight,
+    AlertIOS,
+    Modal
+} from 'react-native';
+
+
+export default class UserGate extends React.Component {
+
+
+    render() {
+
+    }
+}
