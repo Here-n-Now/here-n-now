bc4411e8fd0df7944984cd12af4884854bfd1ff1
Maui21 <ayhamaoui@gmail.com>
2017-05-16 14:46:29 -0400

    Update ViewContainer.js

--- components/ViewContainer.js
+++ components/ViewContainer.js
@@ -69,8 +69,6 @@ export default class ViewContainer extends Component {
   }
 
   render() {
-    console.log('VC props', this.props)
-    console.log('postarr', this.state.postArr)
     const { video, image, text } = this.props.navigation.state.params;
     const { modalVisible, comments, comment } = this.state;
     return  (
